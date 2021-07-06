5543c69bc70a15a78bccc87b480041bad45a80fe
Deatomizeer <56254654+Deatomizeer@users.noreply.github.com>
2021-07-06 14:45:29 +0200

    finished basic simulation loop
    User can now stop and run the simulation by pressing spacebar. When the simulation is frozen, the individual cells can be clicked on to toggle their state.

--- Assets/Scripts/CellManager.cs
+++ Assets/Scripts/CellManager.cs
@@ -1,20 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CellManager : MonoBehaviour
 {
-    private bool alive;
+    // An array storing the materials for cell's states (alive/dead).
+    public Material[] materials = new Material[2];
+    // Whether the cell is alive; also doubles as index for the `materials` array.
+    public int alive;
+    // Cell's renderer reference to swap its material when switching states.
+    Renderer renderer;
+    // Manager script, used to validate clicking on the individual cubes.
+    GameManagerScript managerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        alive = false;
+        alive = 0;
+        renderer = GetComponent<Renderer>();
+        renderer.enabled = true;
+        renderer.sharedMaterial = materials[alive];
+        managerScript = GameObject.Find("GameOfLifeManager").GetComponent<GameManagerScript>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Toggle the life of the selected cell. Works only when the game is paused.
+    private void OnMouseDown()
     {
-        
+        if (!managerScript.running)
+        {
+            alive = (alive + 1) % 2;
+            renderer.sharedMaterial = materials[alive];
+        }
+    }
 
+    // Set the cell to be either alive or dead. `state` can be either 0 or 1.
+    public void setState(int state)
+    {
+        Debug.Assert(state == 0 || state == 1);
+        alive = state;
+        renderer.sharedMaterial = materials[state];
     }
 }
--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    // Cell object.
     public GameObject cubePrefab;
-    [Range(1, 10)]
+    [Range(1, 20)]
     public int gridWidth;
-    [Range(1, 10)]
+    [Range(1, 20)]
     public int gridHeight;
-    [Range(1, 4)]
+    [Range(1, 4)] // Distance coefficient of the `gridSpread` struct.
     public int gridSpreadRatio;
+    [Range(.3f, 1)] // Time per each generation in seconds.
+    public float frameDelay;
+    // Time elapsed since last grid update.
+    private float timer;
 
     private struct GS // Distance between the cubes on the grid.
     {
@@ -18,8 +23,11 @@ public class GameManagerScript : MonoBehaviour
         public float y;
     }
     private GS gridSpread;
-
+    public bool running;
+    // Used to instantiate the objects.
     private GameObject[,] cubeGrid;
+    // Used to set individual cells' states.
+    private CellManager[,] cellScriptGrid;
     // R reset
     // SPACE toggle run
     // Left Click toggle individual cells
@@ -30,9 +38,12 @@ public class GameManagerScript : MonoBehaviour
         Vector3 cubePrefabSize = cubePrefab.GetComponent<MeshFilter>().sharedMesh.bounds.size;
         gridSpread.x = cubePrefabSize.x * gridSpreadRatio;
         gridSpread.y = cubePrefabSize.y * gridSpreadRatio;
+        running = false;
+        timer = 0f;
         // Generate the grid of cubes.
         cubeGrid = new GameObject[gridHeight, gridWidth];
         InstantiateGrid(gridWidth, gridHeight);
+        CreateCellScriptGrid();
     }
 
     private void InstantiateGrid(int width, int height)
@@ -42,10 +53,9 @@ public class GameManagerScript : MonoBehaviour
         {
             for (int j = 0; j < width; j++)
             {
-                cubeGrid[i,j] = Instantiate(cubePrefab, spawnPos, Quaternion.identity);
+                cubeGrid[i, j] = Instantiate(cubePrefab, spawnPos, Quaternion.identity);
                 cubeGrid[i, j].transform.SetParent(this.transform);
                 cubeGrid[i, j].name = "Cell (" + j + ", " + i + ")";
-                //cubeGrid[i, j].GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
                 spawnPos.x += gridSpread.x;
             }
             spawnPos.x = transform.position.x;
@@ -53,9 +63,124 @@ public class GameManagerScript : MonoBehaviour
         }
     }
 
+    // Populate the `cellScriptGrid` array for ease of access to the cells' states.
+    private void CreateCellScriptGrid()
+    {
+        cellScriptGrid = new CellManager[gridHeight, gridWidth];
+        for (int i = 0; i < gridHeight; i++)
+        {
+            for (int j = 0; j < gridWidth; j++)
+            {
+                cellScriptGrid[i, j] = cubeGrid[i, j].GetComponent<CellManager>();
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            running = !running;
+            timer = 0f;
+        }
+        if(running)
+        {
+            timer += Time.deltaTime;
+            if (timer > frameDelay)
+            {
+                int[,] nextGenInts = CalculateNextGeneration();
+                UpdateGrid(nextGenInts);
+
+                timer -= frameDelay;
+            }
+        }
+    }
+
+    // Return a 2D boolean array that determines the next generation layout.
+    private int[,] CalculateNextGeneration()
+    {
+        int[,] nextGen = new int[gridHeight, gridWidth];
+        for (int i = 0; i < gridHeight; i++)
+        {
+            for (int j = 0; j < gridWidth; j++)
+            {
+                int count = CountAliveNeighbors(i, j);
+                int currentState = cellScriptGrid[i, j].alive;
+                nextGen[i, j] = DetermineCellState(count, currentState);
+            }
+        }
+        return nextGen;
+    }
+    // Count the living neighbors of a given cell.
+    private int CountAliveNeighbors(int row, int col)
+    {
+        int count = 0;
+        if(row > 0)
+        {
+            // Top middle.
+            count += cellScriptGrid[row - 1, col].alive;
+            if(col > 0)
+            {
+                // Top left.
+                count += cellScriptGrid[row - 1,col - 1].alive;
+            }
+            if(col < gridWidth-1)
+            {
+                // Top right.
+                count += cellScriptGrid[row - 1, col + 1].alive;
+            }
+        }
+        if (col > 0)
+        {
+            // Middle left.
+            count += cellScriptGrid[row, col - 1].alive;
+        }
+        if (col < gridWidth-1)
+        {
+            // Middle right.
+            count += cellScriptGrid[row, col + 1].alive;
+        }
+        if(row < gridHeight-1)
+        {
+            // Bottom middle.
+            count += cellScriptGrid[row + 1, col].alive;
+            if (col > 0)
+            {
+                // Bottom left.
+                count += cellScriptGrid[row + 1, col - 1].alive;
+            }
+            if (col < gridWidth-1)
+            {
+                // Bottom right.
+                count += cellScriptGrid[row + 1, col + 1].alive;
+            }
+        }
+        return count;
+    }
+    // Determine whether the given cell should live or die in the next generation.
+    private int DetermineCellState(int aliveNeighbors, int currentState)
+    {
+        switch (aliveNeighbors)
+        {
+            case 3:
+                return 1;
+            case 2:
+                return currentState;
+            default:
+                return 0;
+        }
+    }
+    // Proceed into the next generation.
+    private void UpdateGrid(int[,] nextGenInts)
+    {
+        for (int i = 0; i < gridHeight; i++)
+        {
+            for (int j = 0; j < gridWidth; j++)
+            {
+                int state = nextGenInts[i, j];
+                cellScriptGrid[i, j].setState(state);
+            }
+        }
     }
 }
